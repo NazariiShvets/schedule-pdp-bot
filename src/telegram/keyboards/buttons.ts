c55db98482aad7a6b0bbd283e80e0afb96f44d01
Nazarii Shvets <74127809+NazariiShvets@users.noreply.github.com>
2021-03-31 00:53:12 +0300

    add static buttons

--- src/telegram/keyboards/buttons.ts
+++ src/telegram/keyboards/buttons.ts
@@ -40,37 +40,37 @@ const camelize = (text: string) =>
     )
     .join("");
 
-const {
-  backToMainMenuButton,
-  continueButton,
-  fifthPairButton,
-  firstPairButton,
-  fourthPairButton,
-  fridayButton,
-  mondayButton,
-  pairCreateButton,
-  pairDeleteButton,
-  pairEditButton,
-  pairEditClassroomButton,
-  pairEditSubjectButton,
-  pairEditTeacherButton,
-  scheduleButton,
-  scheduleTodayButton,
-  scheduleWeekButton,
-  secondPairButton,
-  sendAuthorMessageButton,
-  sixthPairButton,
-  thirdPairButton,
-  thursdayButton,
-  tuesdayButton,
-  wednesdayButton,
-} = Object.entries(BUTTONS).reduce(
+const objWithButtons = Object.entries(BUTTONS).reduce(
   (buttons, [key, text]) => ({
     ...buttons,
     [`${camelize(key)}Button`]: createButton(text),
   }),
   {} as Record<string, TelegramKeyboardButton>
 );
+
+const backToMainMenuButton = createButton(BUTTONS.BACK_TO_MAIN_MENU);
+const continueButton = createButton(BUTTONS.CONTINUE);
+const firstPairButton = createButton(BUTTONS.FIRST_PAIR);
+const fourthPairButton = createButton(BUTTONS.FOURTH_PAIR);
+const fifthPairButton = createButton(BUTTONS.FIFTH_PAIR);
+const fridayButton = createButton(BUTTONS.FRIDAY);
+const mondayButton = createButton(BUTTONS.MONDAY);
+const pairCreateButton = createButton(BUTTONS.PAIR_CREATE);
+const pairDeleteButton = createButton(BUTTONS.PAIR_DELETE);
+const pairEditButton = createButton(BUTTONS.PAIR_EDIT);
+const pairEditClassroomButton = createButton(BUTTONS.PAIR_EDIT_CLASSROOM);
+const pairEditSubjectButton = createButton(BUTTONS.PAIR_EDIT_SUBJECT);
+const pairEditTeacherButton = createButton(BUTTONS.PAIR_EDIT_TEACHER);
+const scheduleButton = createButton(BUTTONS.SCHEDULE);
+const scheduleTodayButton = createButton(BUTTONS.SCHEDULE_TODAY);
+const scheduleWeekButton = createButton(BUTTONS.SCHEDULE_WEEK);
+const secondPairButton = createButton(BUTTONS.SECOND_PAIR);
+const sendAuthorMessageButton = createButton(BUTTONS.SEND_AUTHOR_MESSAGE);
+const sixthPairButton = createButton(BUTTONS.SIXTH_PAIR);
+const thirdPairButton = createButton(BUTTONS.THIRD_PAIR);
+const thursdayButton = createButton(BUTTONS.THURSDAY);
+const tuesdayButton = createButton(BUTTONS.THUESDAY);
+const wednesdayButton = createButton(BUTTONS.WEDNESDAY);
 
 export {
   BUTTONS,
