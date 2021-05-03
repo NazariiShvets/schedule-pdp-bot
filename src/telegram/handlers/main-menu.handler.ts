8405b1c05a737f87953c4cc2c806bc340a6ba41e
Nazarii Shvets <74127809+NazariiShvets@users.noreply.github.com>
2021-05-03 20:50:04 +0300

    Rework

--- src/telegram/handlers/main-menu.handler.ts
+++ src/telegram/handlers/main-menu.handler.ts
@@ -1,51 +1,16 @@
-import { IUser, UserController } from "../../db";
 import { TelegramAPI } from "../../api";
-import { STATES } from "../types";
 import { defaultHandler } from "./default.handler";
-import { errorKeyboard, mainKeyboard, scheduleKeyboard } from "../keyboards";
+import { scheduleKeyboard } from "../new_keyboards";
 
-const mainMenuHandler = async (chatId: number, user: IUser, text = "") => {
+const mainMenuHandler = async (chatId: number) => {
   try {
-    switch (true) {
-      case mainKeyboard[0][0].text.includes(text): {
-        await UserController.updateUser(user.telegramId, {
-          state: { state: STATES.SCHEDULE },
-        });
-
-        await TelegramAPI.sendMessage(chatId, {
-          text: "Значить розклад... Вибирай",
-          reply_markup: {
-            keyboard: scheduleKeyboard,
-            one_time_keyboard: true,
-            resize_keyboard: true,
-          },
-        });
-
-        break;
-      }
-
-      case mainKeyboard[1][0].text.includes(text): {
-        await UserController.updateUser(user.telegramId, {
-          state: { state: STATES.START },
-        });
-
-        await TelegramAPI.sendMessage(chatId, {
-          text: "На жаль, розробнику писати заборонено",
-          reply_markup: {
-            keyboard: errorKeyboard,
-            one_time_keyboard: true,
-            resize_keyboard: true,
-          },
-        });
-        break;
-      }
-      default: {
-        await defaultHandler(chatId);
-      }
-    }
+    await TelegramAPI.sendMessage(chatId, {
+      text: "Значить розклад... Вибирай",
+      reply_markup: {
+        inline_keyboard: scheduleKeyboard,
+      },
+    });
   } catch (error) {
-    console.log(error);
-
     await defaultHandler(chatId);
   }
 };
