75377dcb16253f1b6cb830d1f13c87cb1308abb2
Nazarii Shvets <74127809+NazariiShvets@users.noreply.github.com>
2021-05-03 21:18:20 +0300

    Remove unused handlers

--- src/telegram/handlers/index.ts
+++ src/telegram/handlers/index.ts
@@ -1,5 +1,3 @@
 export * from "./default.handler";
-export * from "./start.handler";
 export * from "./initial.handler";
 export * from "./main-menu.handler";
-export * from "./schedule";
--- src/telegram/handlers/schedule/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./schedule.handler";
-export * from "./schedule-day.handler";
-export * from "./schedule-week.handler";
-export * from "./schedule-day-create-pair.handler";
