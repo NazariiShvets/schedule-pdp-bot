b48c692725a3c0a35bfa5869a79bf339df619c88
Nazarii Shvets <74127809+NazariiShvets@users.noreply.github.com>
2021-05-03 19:52:08 +0300

    Rework webhook handler

--- src/telegram/webhook.ts
+++ src/telegram/webhook.ts
@@ -1,26 +1,17 @@
 import { APIGatewayProxyEvent } from "aws-lambda";
-import {
-  defaultHandler,
-  initialHandler,
-  mainMenuHandler,
-  scheduleDayHandler,
-  scheduleHandler,
-  startHandler,
-  scheduleWeekHandler,
-} from "./handlers";
+import { defaultHandler, initialHandler, mainMenuHandler } from "./handlers";
 import { TelegramBody } from "../api";
 import { db, UserController } from "../db";
-import { STATES } from "./types";
 
 const webhook = async (event: APIGatewayProxyEvent) => {
   try {
     await db.sync({ force: false });
 
     if (event.body) {
-      const { message }: TelegramBody = JSON.parse(event.body);
+      const { message, callback_query }: TelegramBody = JSON.parse(event.body);
 
       if (message) {
-        const { chat, from, text } = message;
+        const { chat, from } = message;
         const user = await UserController.getUser(from.id);
 
         if (!user) {
@@ -28,41 +19,24 @@ const webhook = async (event: APIGatewayProxyEvent) => {
 
           return { statusCode: 200 };
         }
-        console.log(user);
+      }
 
-        switch (user.state.state) {
-          case STATES.START:
-            await startHandler(chat.id, user, text);
+      if (callback_query) {
+        const { from, data } = callback_query;
 
-            break;
+        const user = await UserController.getUser(from.id);
 
-          case STATES.MAIN_MENU: {
-            await mainMenuHandler(chat.id, user, text);
+        console.log(data);
 
-            break;
+        if (user) {
+          switch (data) {
+            default: {
+              await defaultHandler(from.id);
+            }
           }
-
-          case STATES.SCHEDULE: {
-            await scheduleHandler(chat.id, user, text);
-
-            break;
-          }
-
-          case STATES.SCHEDULE_WEEK: {
-            await scheduleWeekHandler(chat.id, user, text);
-
-            break;
-          }
-
-          case STATES.SCHEDULE_DAY: {
-            await scheduleDayHandler(chat.id, user, text);
-
-            break;
-          }
-
-          default:
-            await defaultHandler(chat.id, user);
         }
+
+        await defaultHandler(from.id);
       }
     }
   } catch (error) {
